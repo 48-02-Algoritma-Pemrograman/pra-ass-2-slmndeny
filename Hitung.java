3202398c24d0e24ec4176273e5b2ccb72d05e371
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-12-11 22:56:40 +0700

    Update Hitung.java

--- Hitung.java
+++ Hitung.java
@@ -4,12 +4,58 @@ import java.util.ArrayList;
 public class Hitung{
 
     public static ArrayList<Integer> data = new ArrayList<>();
+
+    public static double rataRata(ArrayList<Integer>data){
+        double total = 0;
+        for (int i = 0; i < data.size(); i++) {
+                total += data.get(i);
+            }
+        double rata = total / data.size();
+        return rata;
+    }
+
+    public static int nilaiMin (ArrayList<Integer>data){
+        int min = data.get(0);
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i) < min) {
+                min = data.get(i);
+                }
+                }
+                return min;
+    }
+    
+    public static int nilaiMax (ArrayList<Integer>data){
+        int max = data.get(0);
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i) > max) {
+                max = data.get(i);
+                }
+                }
+                return max;
+    }
+
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         /* Pekerjaan anda mulai dari sini */
 
+        String jawab;
+
+        do {
+            data.add(input.nextInt());
+
+            jawab = input.next().trim().toUpperCase();
+            while (!jawab.equals("Y") && !jawab.equals("T")) {
+                jawab = input.next().trim().toUpperCase();
+            }
+        } while (jawab.equals("Y"));
+
+        System.out.println("Nilai minimal : "+ nilaiMin(data));
+        System.out.println("Nilai maksimal : "+ nilaiMax(data));
+        System.out.println("Nilai rata-rata : "+ rataRata(data));
+
         /* Pekerjaan anda berakhir sini */
-        System.out.println(Hitung.class.getDeclaredMethods().length);
-        System.out.println(Hitung.class.getDeclaredFields().length);
+
+        input.close();
     }
 }
